76786bdd0a8b607ea1dd750b8c6c7b0d7534378f
Ad van der Veer <advanderveer@gmail.com>
2021-03-16 09:42:20 +0100

    moved template renderer to different directory

--- rbview/README.md
+++ rbview/README.md
@@ -1,3 +1,6 @@
 # Backlogs
 - [ ] COULD add a top level Render that either uses the view configured in the context, or a global view if
             non is available. BUT 1: view is not request scoped, BUT 2: globals are an anti-pattern
+- [ ] SHOULD add a renderer for the "session" that "renders" by saving the session to the response. It must
+            be placed before regular renderers.
+- [ ] SHOULD move all template stuff to rbtemplate
--- rbview/view.go
+++ rbview/view.go
@@ -1,61 +1,11 @@
 package rbview
 
-import (
-	"embed"
-	"html/template"
-	"io/fs"
-	"os"
+import "net/http"
 
-	"github.com/caarlos0/env/v6"
-	"go.uber.org/fx"
-	"go.uber.org/zap"
-)
-
-// Conf configures the view
-type Conf struct {
-	Dir      string   `env:"RB_VIEW_DIR" envDefault:"."`
-	Patterns []string `env:"RB_VIEW_PATTERNS" envSeparator:":" envDefault:"*.html"`
-	Name     string   `env:"RB_VIEW_NAME" envDefault:"root"`
+type Encoder interface {
+	Encode(w http.ResponseWriter, r *http.Request) error
 }
 
-// ParseConf parses the env
-func ParseConf() (cfg Conf, err error) {
-	return cfg, env.Parse(&cfg)
-}
-
-// TemplateFiles are on a filesystem
-type TemplateFiles fs.FS
-
-// FromEmbed creates a template files fs from an embedded filesystem
-func FromEmbed(efs embed.FS) TemplateFiles { return efs }
-
-// FromDir provides template files from an actual directory
-func FromDir(logs *zap.Logger, cfg Conf) TemplateFiles {
-	logs.Info("configure dir fs", zap.String("dir", cfg.Dir))
-	return os.DirFS(cfg.Dir)
-}
-
-// Params are parameters for view construction
-type Params struct {
-	fx.In
-	Files TemplateFiles
-	Funcs []template.FuncMap `group:"rb.helper"`
-}
-
-// New creates the view templates
-func New(logs *zap.Logger, cfg Conf, p Params) (*template.Template, error) {
-	tmpl, hnames := template.New(cfg.Name), []string{}
-	for _, fm := range p.Funcs {
-		tmpl = tmpl.Funcs(fm)
-		for fname := range fm {
-			hnames = append(hnames, fname)
-		}
-	}
-
-	logs.Info("parse templates from fs",
-		zap.String("name", tmpl.Name()),
-		zap.Strings("patterns", cfg.Patterns),
-		zap.Strings("helpers", hnames))
-
-	return tmpl.ParseFS(p.Files, cfg.Patterns...)
+type View struct {
+	encs []Encoder
 }
